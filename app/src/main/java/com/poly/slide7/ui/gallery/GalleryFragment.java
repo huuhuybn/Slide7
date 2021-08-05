ef7ebbddd08cc8de97c41f2b44a3596e2fdd3323
huuhuybnPC <huuhuybn@gmail.com>
2021-08-05 10:42:21 +0700

    Initial commit

---
+++ app/src/main/java/com/poly/slide7/ui/gallery/GalleryFragment.java
@@ -0,0 +1,29 @@
+package com.poly.slide7.ui.gallery;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.poly.slide7.R;
+import com.poly.slide7.databinding.FragmentGalleryBinding;
+
+public class GalleryFragment extends Fragment {
+
+
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_gallery, container, false);
+        
+        return root;
+    }
+
+}
